7b3a13a78be4e2001970405432b18c529199a55f
Remus Baciu <remus.baciu@sky.uk>
2025-07-02 14:13:48 +0100

    Initial setup of react + vite + ts + mantine frontend project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button, Container, Title } from '@mantine/core';
+
+function App() {
+    return (
+        <Container>
+            <Title order={1}>Welcome to Mantine</Title>
+            <Button mt="md">Click me</Button>
+        </Container>
+    );
+}
+
+export default App;
